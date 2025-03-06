106a05e3294b85585f7164137818d318d17fb2cf
nikolaos83 <54727787+nikolaos83@users.noreply.github.com>
2025-03-06 14:50:30 +0300

    Add files via upload

---
+++ check-enable-internet.bat
@@ -0,0 +1,15 @@
+set /a test=0
+
+:InternetTesti
+set /a test=%test%+1
+powershell -command "invoke-webrequest http://www.msftconnecttest.com/connecttest.txt -outfile '%Temp%\internet.txt'"
+
+dir "%Temp%\internet.txt"
+
+If %errorlevel% EQU 0 (del "%Temp%\internet.txt") & (goto testBitti)
+If %test% LSS 3 (timeout 3) & goto InternetTesti
+
+netsh interface set interface "Wi-Fi" disabled
+netsh interface set interface "Wi-Fi" enabled
+
+:testBitti
